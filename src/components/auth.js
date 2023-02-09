10fcea5fc9d8e049bbcaa4fe9c30a76b18116393
ayaIdd <you@example.com>
2023-02-09 20:58:14 +0100

    first commit

---
+++ src/components/auth.js
@@ -0,0 +1,48 @@
+import {auth , googleProvider} from '../config/firebase-config';
+import {createUserWithEmailAndPassword ,signInWithPopup,signOut} from 'firebase/auth'
+import {useState} from 'react';
+
+export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const SignIn = async () => {
+        try{
+      await createUserWithEmailAndPassword(auth,email,password);
+        }catch(err)
+        {
+            console.error(err);
+        }
+     };
+
+     const SignInWithGoogle = async () => {
+        try{
+      await signInWithPopup(auth,googleProvider);
+        }catch(err)
+        {
+            console.error(err);
+        }
+     };   
+
+     const LogOut = async () => {
+        try{
+      await signOut(auth);
+        }catch(err)
+        {
+            console.error(err);
+        }
+     };   
+     
+      
+    return (
+        <div>
+            <input placeholder="Email .." onChange={ (e) => setEmail(e.target.value)}/>
+            <input placeholder="password .." onChange={(e)=>setPassword(e.target.value) }type="password"/>
+
+            <button onClick={SignIn} >Sign in </button>
+            <button onClick={SignInWithGoogle}> Sign in with google</button>
+            <button onClick={LogOut}> logout</button>
+            
+            
+        </div>
+    );
+};
---
+++ src/config/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth,GoogleAuthProvider } from 'firebase/auth'
+const firebaseConfig = {
+  apiKey: "AIzaSyAR81L9YOTKX0Y3tDVRiNgYqs1Shb_qXfc",
+  authDomain: "freact-ee258.firebaseapp.com",
+  projectId: "freact-ee258",
+  storageBucket: "freact-ee258.appspot.com",
+  messagingSenderId: "71931694527",
+  appId: "1:71931694527:web:d5f49fd92273c3843fe50a",
+  measurementId: "G-SS9VNP4LRL"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ const auth = getAuth(app);
+ const googleProvider = new  GoogleAuthProvider();
+ export {auth , googleProvider};
+
